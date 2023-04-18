8e9840a1567afcf1946dc888adf44ab194a187fd
huseyn123 <mailofhuseyn@gmail.com>
2023-04-19 02:46:25 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CheckAut();
+
+        }
+
+      
+        public static void CheckAut()
+        {
+            do
+            {
+                Console.WriteLine("Istifadeci Adi Daxil Edin:");
+                string username = Console.ReadLine();
+                Console.WriteLine("Parol Daxil Edin:");
+                string password = Console.ReadLine();
+
+                if (username == "admin" && password == "12345")
+                {
+                    Console.WriteLine("Authed");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Failed auth");
+                }
+
+            } while (true);
+        }
+
+
+
+    }
+}
